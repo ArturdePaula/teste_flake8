828ac6ea3c5c9867dab8f75315563ba35f6e1521
Artur Oliveira de Paula <Artur.olidepaula@gmail.com>
2023-08-03 11:17:20 -0300

    first commit

---
+++ fila_normal.py
@@ -0,0 +1,30 @@
+class filanormal: 
+    codigo: int  = 0 
+    fila = []
+    clientesatendidos = []
+    senhatual : str = " "
+
+
+    def gerasenhaatual (self) -> None: 
+        """Metodo/Função que gera a senha atua, concatenanto o prefixo NM com o codigo da posição na fila"""
+        self.senhatual = f'NM {self.codigo}'
+    
+    def resetafila ( self ) -> None:
+        """Metodo/função que reseta a fila para começar do 0 novamente se o codigo ( posição na fila ) já for de 100, caso contrario soma + 1 na codigo da fila"""
+        if ( self.codigo == 100): 
+            self.codigo = 0
+        else: 
+            self.codigo += 1
+
+    def atualizafila ( self ) -> None:
+        """Metodo/Função para quando um clinte requisita uma senha, aumentando o codigo da fila, gerando sua senha e colocando o mesmo na fila"""
+        self.resetafila ()
+        self.gerasenhaatual ()
+        self.fila.append ( self.senhatual )
+
+    def chamaCliente ( self, caixa : int ) -> str:
+        """Metodo/Função que retorna uma String informando o codigo do primeiro cliente e o numero do caixa que esta chamando para atendimento. Após isso
+        a senha do cliente atendido e colocada na lista de clientes atendidos """ 
+        cliente_atual = self.fila.pop (0)
+        self.clientesatendidos.append ( cliente_atual )
+        return (f'Cliente de codigo { cliente_atual }, dirija-se ao caixa {caixa}')
---
+++ main.py
@@ -0,0 +1,29 @@
+# Importando as duas filas de seus determinados documentos
+from fila_normal import filanormal
+from fila_prioritaria import FilaPrioritaria
+
+# Criando objetos das duas filas existentes
+fila_teste1 = filanormal()
+fila_teste2 = FilaPrioritaria()
+
+
+# Atualizando as filas 3 vezes cada uma
+fila_teste1.atualizafila()
+fila_teste1.atualizafila()
+fila_teste1.atualizafila()
+
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+
+# Chamando os clientes de ambas as filas 3 vezes intercalando nos mesmos caixas
+print(fila_teste1.chamaCliente(10))
+print(fila_teste2.chama_cliente(10))
+
+print(fila_teste1.chamaCliente(11))
+print(fila_teste2.chama_cliente(11))
+
+print(fila_teste1.chamaCliente(12))
+print(fila_teste2.chama_cliente(12))
+
+print(f'As estatisticas dos clintes da fila prioritarias foram as seguintes: {fila_teste2.estatistica (5, 219, "not_detail" )}')
